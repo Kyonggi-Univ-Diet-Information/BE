499e2227a037c71e77b1f3176fd9bee6ab038065
boroboro01 <duke7201@naver.com>
2024-09-10 11:27:32 +0900

    feat: add validation to memember request

--- src/main/java/com/kyonggi/diet/member/io/MemberRequest.java
+++ src/main/java/com/kyonggi/diet/member/io/MemberRequest.java
@@ -1,5 +1,8 @@
 package com.kyonggi.diet.member.io;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,10 +14,16 @@ import lombok.NoArgsConstructor;
 @Builder
 public class MemberRequest {
 
+    @NotBlank(message = "Email is required")
+    @Email(message = "Provide valid email address")
     private String email;
 
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password should be at least 8 characters")
     private String password;
 
+    @NotBlank(message = "Name is required")
+    @Size(max = 10, message = "Name should be at most 10 characters")
     private String name;
 
     private String profileUrl;

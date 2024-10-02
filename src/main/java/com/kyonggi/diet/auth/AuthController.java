31de975a63236b1bce24e236c38429b44d9faf8b
boroboro01 <duke7201@naver.com>
2024-10-02 16:45:16 +0900

    feat: create login end point

--- src/main/java/com/kyonggi/diet/auth/AuthController.java
+++ src/main/java/com/kyonggi/diet/auth/AuthController.java
@@ -1,14 +1,23 @@
 package com.kyonggi.diet.auth;
 
+import com.kyonggi.diet.auth.io.AuthRequest;
+import com.kyonggi.diet.auth.io.AuthResponse;
+import com.kyonggi.diet.auth.util.JwtTokenUtil;
 import com.kyonggi.diet.member.MemberDTO;
 import com.kyonggi.diet.member.io.MemberRequest;
 import com.kyonggi.diet.member.io.MemberResponse;
+import com.kyonggi.diet.member.service.CustomMembersDetailService;
 import com.kyonggi.diet.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @Slf4j
@@ -18,6 +27,9 @@ public class AuthController {
 
     private final MemberService memberService;
     private final ModelMapper modelMapper;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final CustomMembersDetailService membersDetailService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/register")
@@ -27,6 +39,15 @@ public class AuthController {
         memberDTO = memberService.createMember(memberDTO);
         log.info("Member DTO details {}", memberDTO);
         return mapToMemberReponse(memberDTO);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse authenticateProfile(@RequestBody AuthRequest authRequest) {
+        log.info("API POST /login called");
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
+        final UserDetails user = membersDetailService.loadUserByUsername(authRequest.getEmail());
+        final String token = jwtTokenUtil.generateToken(user);
+        return new AuthResponse(token, authRequest.getEmail());
     }
 
 

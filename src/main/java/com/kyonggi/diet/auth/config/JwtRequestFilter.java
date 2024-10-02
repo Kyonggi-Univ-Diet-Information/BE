b914704b3a4f0cf1cb5261bbb054854647c3a922
boroboro01 <duke7201@naver.com>
2024-10-02 17:31:08 +0900

    feat: create validation jwt token

--- src/main/java/com/kyonggi/diet/auth/config/JwtRequestFilter.java
+++ src/main/java/com/kyonggi/diet/auth/config/JwtRequestFilter.java
@@ -1,18 +1,31 @@
 package com.kyonggi.diet.auth.config;
 
 import com.kyonggi.diet.auth.util.JwtTokenUtil;
+import com.kyonggi.diet.member.service.CustomMembersDetailService;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 public class JwtRequestFilter extends OncePerRequestFilter {
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection") // I have to remove this and fix the error
+    @Autowired
     private JwtTokenUtil jwtTokenUtil;
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection") // I have to remove this and fix the error
+    @Autowired
+    private CustomMembersDetailService membersDetailService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -31,7 +44,17 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             } catch (ExpiredJwtException ex) {
                 throw new RuntimeException("Jwt Token is expired", ex);
             }
-
         }
+
+        if(email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = membersDetailService.loadUserByUsername(email);
+
+            if(jwtTokenUtil.validateToken(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 }

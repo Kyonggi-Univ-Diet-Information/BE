4f4c50f4527351c0a6a1a52ef3088d729d17884f
boroboro01 <duke7201@naver.com>
2024-11-12 17:17:23 +0900

    feat: create kakao auth controller

--- src/main/java/com/kyonggi/diet/auth/KakaoAuthController.java
+++ src/main/java/com/kyonggi/diet/auth/KakaoAuthController.java
@@ -1,12 +1,15 @@
 package com.kyonggi.diet.auth;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
 @Slf4j
@@ -16,13 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class KakaoAuthController {
 
     @Value("${kakao.client_id}")
-    private String client_id;
+    private String clientId;
 
     @Value("${kakao.redirect_uri}")
-    private String redirect_uri;
+    private String redirectUri;
 
     @GetMapping("/kakao-login")
-    public String login() {
-        return "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id="+client_id+"&redirect_uri="+redirect_uri;
+    public String kakaoLogin(@RequestParam(value="code", required = false) String code, HttpSession session, RedirectAttributes rttr) throws Exception {
+        log.info("#######{}", code);
+        String accessToken = kakaoAuthService.getAccessToken(code);
+        return kakaoAuthService.getUserInfo(accessToken, session, rttr);
     }
 }

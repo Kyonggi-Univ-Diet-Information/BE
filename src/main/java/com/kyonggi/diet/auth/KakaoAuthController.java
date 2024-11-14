f5cf74ed0c41645c0426eff6f4ab3d29c8e4b02f
boroboro01 <duke7201@naver.com>
2024-11-14 22:46:51 +0900

    feat : inject dependency to kakao-auth-controller

--- src/main/java/com/kyonggi/diet/auth/KakaoAuthController.java
+++ src/main/java/com/kyonggi/diet/auth/KakaoAuthController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -16,7 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Tag(name = "카카오 인증 API", description = "카카오 소셜 회원 인증(회원가입, 로그인 등)에 대한 API 입니다.")
 public class KakaoAuthController {
 
-    private KakaoAuthService kakaoAuthService;
+    final private KakaoAuthService kakaoAuthService;
 
     @Value("${kakao.client_id}")
     private String clientId;

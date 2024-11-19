67f998252dbd6f1f9baf91de48e6b351982e9420
boroboro01 <duke7201@naver.com>
2024-11-20 02:22:06 +0900

    feat : remove logic to check password for getting user data

--- src/main/java/com/kyonggi/diet/member/service/CustomMembersDetailService.java
+++ src/main/java/com/kyonggi/diet/member/service/CustomMembersDetailService.java
@@ -23,8 +23,15 @@ public class CustomMembersDetailService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // 이메일로 MemberEntity 조회
         MemberEntity memberEntity = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("Member not found for the email" + email));
-        return new User(memberEntity.getEmail(), memberEntity.getPassword(), new ArrayList<>());
+                .orElseThrow(() -> new UsernameNotFoundException("Member not found for the email: " + email));
+
+        // 비밀번호가 없는 경우 기본값 설정
+        String password = memberEntity.getPassword() != null ? memberEntity.getPassword() : "SOCIAL_LOGIN_PASSWORD";
+
+        // UserDetails 반환
+        return new User(memberEntity.getEmail(), password, new ArrayList<>());
     }
+
 }

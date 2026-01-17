31ab4854a0244d3934f7517705f39ffff70ba568
HongSeongJo <164273757+HongSeongJo@users.noreply.github.com>
2026-01-17 14:46:34 +0900

    mistake

--- src/main/java/com/kyonggi/diet/auth/socialRefresh/SocialRefreshToken.java
+++ src/main/java/com/kyonggi/diet/auth/socialRefresh/SocialRefreshToken.java
@@ -1,4 +1,38 @@
 package com.kyonggi.diet.auth.socialRefresh;
 
+import com.kyonggi.diet.auth.Provider;
+import com.kyonggi.diet.member.MemberEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class SocialRefreshToken {
+    @Id @Column(name = "member_id")
+    private Long memberId;
+
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "member_id",
+            foreignKey = @ForeignKey(name = "fk_member_social_refresh")
+    )
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private MemberEntity member;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Provider provider;
+
+    private String refreshToken;
+
+    public void updateRefreshToken(String refreshTokenValue) {
+        this.refreshToken = refreshTokenValue;
+    }
 }
